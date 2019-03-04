03c397722323d437609c05a92853f036b8b6d1f1
Anil Harish <anil.harish@outlook.com>
2019-03-03 18:45:31 -0700

    Create your own smart pointer with move semantics

---
+++ Intermediate/my_smartpointer.cpp
@@ -0,0 +1,66 @@
+#include "pch.h"
+#include <iostream>
+
+template<class PTR>
+class My_smartptr
+{
+	PTR* m_ptr;
+public:
+	My_smartptr(PTR* ptr = nullptr)
+		:m_ptr(ptr)
+	{
+	}
+
+	~My_smartptr()
+	{
+		delete m_ptr;
+	}
+
+	// A copy constructor that implements move semantics
+	My_smartptr(My_smartptr& a) // note: not const
+	{
+		m_ptr = a.m_ptr; // transfer our dumb pointer from the source to our local object
+		a.m_ptr = nullptr; // make sure the source no longer owns the pointer
+	}
+
+	// An assignment operator that implements move semantics
+	My_smartptr& operator=(My_smartptr& a) // note: not const
+	{
+		if (&a == this)
+			return *this;
+
+		delete m_ptr; // make sure we deallocate any pointer the destination is already holding first
+		m_ptr = a.m_ptr; // then transfer our dumb pointer from the source to the local object
+		a.m_ptr = nullptr; // make sure the source no longer owns the pointer
+		return *this;
+	}
+
+	PTR& operator*() const { return *m_ptr; }
+	PTR* operator->() const { return m_ptr; }
+	bool isNull() const { return m_ptr == nullptr; }
+};
+
+class Resource
+{
+public:
+	Resource() { std::cout << "Resource acquired\n"; }
+	~Resource() { std::cout << "Resource destroyed\n"; }
+};
+
+int main()
+{
+	My_smartptr<Resource> res1(new Resource);
+	My_smartptr<Resource> res2; // Start as nullptr
+
+	std::cout << "res1 is " << (res1.isNull() ? "null\n" : "not null\n");
+	std::cout << "res2 is " << (res2.isNull() ? "null\n" : "not null\n");
+
+	res2 = res1; // res2 assumes ownership, res1 is set to null
+
+	std::cout << "Ownership transferred\n";
+
+	std::cout << "res1 is " << (res1.isNull() ? "null\n" : "not null\n");
+	std::cout << "res2 is " << (res2.isNull() ? "null\n" : "not null\n");
+
+	return 0;
+}
